a4a37fcc7d8d32812394df2dff16a0cb6370f0c8
ishaanm612 <darkknightishaan@gmail.com>
2026-02-07 11:33:46 -0500

    fixed pkg config

--- mosaic/next.config.ts
+++ mosaic/next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  transpilePackages: ["react-leaflet"],
+  transpilePackages: ["react-leaflet", "framer-motion"],
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
